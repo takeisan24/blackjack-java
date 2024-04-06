897bd9d80f02344b51d447d796792ef28ebf6d4b
takei_san204 <vutanh507@gmail.com>
2024-04-06 21:55:35 +0700

    Update App & Blackjack.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Blackjack Blackjack = new Blackjack();
+    public static void main(String[] args) {
+        Blackjack blackjack = new Blackjack();
     }
 }
